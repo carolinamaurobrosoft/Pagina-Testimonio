86ecb6077038759f60c5f2cae79cbafbca8cd71e
Carolina Mauro <carolina.mauro@brosoft.com.ar>
2022-06-06 13:17:32 -0300

    Detalles finales

--- src/components/Testimony.js
+++ src/components/Testimony.js
@@ -4,16 +4,19 @@ import '../stylesheet/Testimony.css'
 //definimos mi componente
 
 function Testimony(props){
+  const {
+    name, image,position,testimony, country, company
+  } = props
   return ( //describir la estructura de mi componente
     <div className='container-testimony'> 
       <img 
         className='img-testimony'
-        src={require(`../images/${props.image}-testimony.jpg`)}
-        alt = 'Foto de Persona1'/>
+        src={require(`../images/${image}-testimony.jpg`)}
+        alt = 'Foto de Persona'/>
       <div className='container-text-testimony'>
-        <p className='name-testimony'> <strong> {props.name} </strong>  en {props.country} </p>
-        <p className='position-testimony'> {props.position} en <strong> {props.company} </strong></p>
-        <p className='text-testimony'> "{props.testimony}" </p>
+        <p className='name-testimony'> <strong> {name} </strong>  en {country} </p>
+        <p className='position-testimony'> {position} en <strong> {company} </strong></p>
+        <p className='text-testimony'> "{testimony}" </p>
       </div> 
     
     </div> 
