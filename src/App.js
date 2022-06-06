e41fcbf58c489803dc6b6a8c32e87e2018d84134
Carolina Mauro <carolina.mauro@brosoft.com.ar>
2022-06-06 10:06:22 -0300

    Aplicando estilos aplicacion general

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Testimony from './components/Testimony.js';
 function App() {
   return (
     <div className="App">
+      <div className='main-container'>
+      <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp: </h1>
+      </div>
       <Testimony />
     </div>
   );
